a066f4c154d95840ea81a458f191698d5cbfe17d
Sander Hesselink <sander@hesselink.me>
2026-05-15 04:57:09 +0200

    fsd

--- tag.ps1
+++ tag.ps1
@@ -1,4 +1,4 @@
-$version="1.0.6" # --- UPDATE THIS VERSION NUMBER ---
+$version="1.0.7" # --- UPDATE THIS VERSION NUMBER ---
 git tag -a v$version -m "Release version $version"
 git push origin v$version
 echo "Tag v$version created and pushed to origin."
